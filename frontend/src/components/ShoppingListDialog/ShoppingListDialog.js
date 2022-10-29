d93e328e809ffbf9da290675e49cdc7c68708c53
Nikita <88973749+kinzzzer@users.noreply.github.com>
2022-10-29 14:37:03 +0300

    Add files via upload

--- frontend/src/components/ShoppingListDialog/ShoppingListDialog.js
+++ frontend/src/components/ShoppingListDialog/ShoppingListDialog.js
@@ -64,7 +64,7 @@ export default function ShoppingListDialog(props) {
                   isButtonShown
                 />)
             })
-            : <DialogContentText><PurchaseForm /></DialogContentText>}
+            : <PurchaseForm />}
         </DialogContent>
         <DialogActions>
           <DialogTitle>{currentStepIndex + 1} / {steps.length}</DialogTitle>
